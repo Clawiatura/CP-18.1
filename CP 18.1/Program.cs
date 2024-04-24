1b9d837847f183fc9d0b9295a7b8e0e9222f1711
Clawiatura <maksimov19992805@icloud.com>
2024-04-24 15:55:53 +0200

    Добавьте файлы проекта.

---
+++ CP 18.1/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] forbiddenWord = { "Чебурек", "Шаурма", "Бургер" };
+string sentence = "";
+while (true)
+{
+    Console.WriteLine("Введите слово: (Для выхода введите: выход.)");
+    string word = Console.ReadLine();
+    for (int i = 0;  i < forbiddenWord.Length; i++)
+    {
+        if (forbiddenWord[i] == word)
+        {
+            word = "()";
+        }
+    }
+    if (word == "выход")
+    {
+        break;
+    }
+    sentence += word + " ";
+
+}
+Console.WriteLine(sentence);
